f73622e8c0378a801f1317de726abf516f2c2cae
Natalie <nataliemsbl@gmail.com>
2024-06-20 00:45:07 +0400

    updated pages & styles

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <title>Web Project Demo</title>
     <link rel="stylesheet" href="style.css">
 
-    <link rel="stylesheet" href="other.css">
+    <link rel="stylesheet" href="main.css">
     <!-- <script src="https://kit.fontawesome.com/92d78ba0ea.js" crossorigin="anonymous"></script> -->
 </head>
 <body>
@@ -18,37 +18,34 @@
     <?php include 'nav-bar.php'; ?>
 
     <main id="landing-page">
-        <!-- <img src="images/study.png" alt="image"> -->
         
-        <h1> Develop skills <br>
-             from the best <br>
-             source
-        </h1>
+        <h1> Your Ultimate Resource for Academic Success </h1>
 
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Enim hic illum sed aut quos vero labore excepturi cumque
-            asperiores quae illo ratione, soluta reprehenderit,
-            possimus at qui provident quis repudiandae?
+            Discover, share, and enhance your study materials in one collaborative platform.
+            Join our community of students dedicated to achieving excellence through
+            shared knowledge and support. Whether you're looking for detailed notes or 
+            ready to share your own, Study Buddy is here to make your academic journey
+            smoother and more successful. Start exploring now and take your studies to the next level!
         </p>
 
-        <form name="search">
-            <input type="text" name="txt" onmouseout="this.value = ''; this.blur();">
-            <button> Search </button>
-        </form>
-
+        <a href="about.php"> Learn More </a>
         
+        <img src="images/books.png" alt="image">
         
     </main>
 
-    <main id="popular-posts">
-        <h1> Special <br>
-            features are <br>
-            only for you
-        </h1><!--
+    <aside>
+        <img src="images/study.png" alt="image">
+    </aside>
 
+    <main id="popular-posts">
+        <h1> Stay Updated with <br>
+             the Latest Posts <br>
+             from Your Peers
+        </h1>
         
-        --><div>
+        <div>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Enim hic illum sed aut quos vero labore excepturi cumque
@@ -56,18 +53,18 @@
                 possimus at qui provident quis repudiandae?
             </p>
 
-            <button> View all courses </button>
+            <a href="about.php"> View all posts </a>
         </div>
 
         <div class="post">
             post 1
-        </div><!--
+        </div>
         
-        --><div class="post">
+        <div class="post">
             post 2
-        </div><!--
-
-        --><div class="post">
+        </div>
+        
+        <div class="post">
             post 3
         </div>
     </main>
